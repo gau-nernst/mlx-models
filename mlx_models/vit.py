94fe84331c7de0908094f046060ad6dc5264774b
Thien Tran <gau.nernst@yahoo.com.sg>
2024-03-24 16:18:30 +0800

    add timm weight loading

--- mlx_models/vit.py
+++ mlx_models/vit.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import mlx.core as mx
 from mlx import nn
 
@@ -36,15 +38,19 @@ class Attention(nn.Module):
 
 class Mlp(nn.Module):
     def __init__(
-        self, in_features: int, hidden_features: int | None = None, out_features: int | None = None, act=nn.gelu
+        self,
+        in_features: int,
+        hidden_features: int | None = None,
+        out_features: int | None = None,
+        act_layer: type[nn.Module] = nn.GELU,
     ) -> None:
         super().__init__()
         hidden_features = hidden_features or in_features
         out_features = out_features or in_features
 
         self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
         self.fc2 = nn.Linear(hidden_features, out_features)
-        self.act = act
 
     def __call__(self, x: mx.array) -> mx.array:
         return self.fc2(self.act(self.fc1(x)))
@@ -56,13 +62,14 @@ class ViTBlock(nn.Module):
         dim: int,
         num_heads: int,
         mlp_ratio: float = 4.0,
-        act=nn.gelu,
+        norm_layer: type[nn.Module] = nn.LayerNorm,
+        act_layer: type[nn.Module] = nn.GELU,
     ) -> None:
         super().__init__()
-        self.norm1 = nn.LayerNorm(dim)
+        self.norm1 = norm_layer(dim)
         self.attn = Attention(dim, num_heads)
-        self.norm2 = nn.LayerNorm(dim)
-        self.mlp = Mlp(dim, int(dim * mlp_ratio), act=act)
+        self.norm2 = norm_layer(dim)
+        self.mlp = Mlp(dim, int(dim * mlp_ratio), act_layer=act_layer)
 
     def __call__(self, x: mx.array) -> mx.array:
         x = x + self.attn(self.norm1(x))
@@ -84,12 +91,14 @@ class ViT(nn.Module):
         mlp_ratio: float = 4.0,
         class_token: bool = True,
         fc_norm: bool | None = None,
-        act=nn.gelu,
+        norm_layer: type[nn.Module] = partial(nn.LayerNorm, eps=1e-6),
+        act_layer: type[nn.Module] = nn.GELU,
     ) -> None:
         super().__init__()
         assert global_pool in ("avg", "token")
         assert class_token or global_pool != "token"
         use_fc_norm = global_pool == "avg" if fc_norm is None else fc_norm
+        self.in_chans = in_chans
         self.global_pool = global_pool
         self.num_prefix_tokens = 1 if class_token else 0
 
@@ -97,11 +106,13 @@ class ViT(nn.Module):
         self.cls_token = mx.zeros((1, 1, embed_dim))
 
         n_patches = (img_size // patch_size) ** 2 + 1
-        self.pos_embed = mx.zeros((n_patches, embed_dim))
-        self.blocks = [ViTBlock(embed_dim, num_heads, mlp_ratio, act=act) for _ in range(depth)]
-        self.norm = nn.LayerNorm(embed_dim) if not use_fc_norm else nn.Identity()
+        self.pos_embed = mx.zeros((1, n_patches, embed_dim))
+        self.blocks = [
+            ViTBlock(embed_dim, num_heads, mlp_ratio, norm_layer=norm_layer, act_layer=act_layer) for _ in range(depth)
+        ]
+        self.norm = norm_layer(embed_dim) if not use_fc_norm else nn.Identity()
 
-        self.fc_norm = nn.LayerNorm(embed_dim) if use_fc_norm else nn.Identity()
+        self.fc_norm = norm_layer(embed_dim) if use_fc_norm else nn.Identity()
         self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
     def __call__(self, x: mx.array) -> mx.array:
@@ -120,3 +131,15 @@ class ViT(nn.Module):
             x = x[:, 0]
 
         return self.head(self.fc_norm(x))
+
+    def load_weights(self, file_or_weights: str | list[tuple[str, mx.array]], strict: bool = True) -> None:
+        weights = file_or_weights
+        if isinstance(weights, str):
+            weights = list(mx.load(weights).items())
+
+        # patch PyTorch Conv2d weights
+        for i, (k, v) in enumerate(weights):
+            if k == "patch_embed.proj.weight" and v.shape[1] == self.in_chans:
+                weights[i] = (k, v.transpose(0, 2, 3, 1))
+
+        return super().load_weights(weights, strict)
